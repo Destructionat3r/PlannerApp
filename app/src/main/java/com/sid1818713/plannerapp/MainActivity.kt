91b39f037178b4739bc65f829b27ca53cdbd48ee
Luke Perry <lukeperry7700@gmail.com>
2021-05-18 15:21:20 +0100

    Initial commit

---
+++ app/src/main/java/com/sid1818713/plannerapp/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.sid1818713.plannerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_menu.setNavigationItemSelectedListener(this)
+
+        setToolbarTitle("Home")
+        changeFragment(Home())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        when(item.itemId) {
+            R.id.home -> {
+                setToolbarTitle("Home")
+                changeFragment(Home())
+            }
+
+            R.id.classes -> {
+                setToolbarTitle("Classes")
+                changeFragment(Classes())
+            }
+
+            R.id.notes -> {
+                setToolbarTitle("Notes")
+                changeFragment(Notes())
+            }
+        }
+        return true
+    }
+
+    fun setToolbarTitle(title:String) {
+        supportActionBar?.title = title
+    }
+
+    fun changeFragment(frag : Fragment) {
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.fragment_container, frag).commit()
+    }
+}
---
+++ app/src/main/java/com/sid1818713/plannerapp/data/NoteDao.kt
@@ -0,0 +1,15 @@
+package com.sid1818713.plannerapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun addNote(note: Note)
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun readAllData(): LiveData<List<Note>>
+}
